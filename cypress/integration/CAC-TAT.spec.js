bd18a34c766de436f91c0f95335a5c99c3514142
Renato Mertz <renatomertz@gmail.com>
2022-06-09 16:43:19 -0300

    Rolbacking error simutaion

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,7 +7,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   });
 
   it('verifica o título da aplicação', () => {
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TATs');
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
   });
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
